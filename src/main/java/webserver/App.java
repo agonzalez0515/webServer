c27bd45baef2c2efe2c20108ba8c9ddde68f6485
Angelica Gonzalez <angelica@8thlight.com>
2019-12-02 12:55:54 -0800

    Serving html pages for home, list, and 404
    Change how hash map is created
    
    Add procfile
    
    Fix procfile and jar manifest
    
    Use system port
    
    add CRLF to response
    
    Fix response lines
    
    Add html pages
    
    Refactor main class. Create socket creator class and socket handler class.
    
    Refactor response to follow single responsibility
    
    setup the response data to be sent
    
    update gitignore
    
    Remove commented out tests

--- src/main/java/webserver/App.java
+++ src/main/java/webserver/App.java
@@ -1,58 +1,28 @@
 package webserver;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class App {
 
-    public String getGreeting() {
-        return "Hello world.";
-    }
-
-    public static ServerSocket createServerSocket(int portNum) throws IOException {
-        return new ServerSocket(portNum);
-    }
-
-    public static Socket createClientConnection(ServerSocket server) throws IOException {
-        return server.accept();
-    }
-
-
-
-    public static void run() throws  IOException {
-        ServerSocket server = createServerSocket(5000);
-        Socket client = createClientConnection(server);
-        BufferedReader in = SocketIO.createSocketReader(client);
-        PrintWriter out = SocketIO.createSocketWriter(client);
-
-        while (client != null) {
-            Request request = new Request(in);
-            if (request.parse())  {
-                String method = request.method;
-                String path = request.path;
-                System.out.println("in app " + path);
-                Response response = new Response(out, method, path);
-                response.send();
-
-            } else {
-                String responseLine = "HTTP/1.1 " + 500 + " " + "Unable to parse request" + "\r\n\r\n";
-                out.println(responseLine);
-                return;
-            }
-        }
-    }
-
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
         try {
-            run();
+            ServerSocket server = SocketCreator.createServerSocket();
+
+            while (true) {
+                Socket client = SocketCreator.createClientConnection(server);
+                Thread serverThread = new Thread(new SocketHandler(client));
+                serverThread.start();
+            }
+
         } catch(IOException e) {
             e.printStackTrace();
         }
     }
+
 }
+
+
