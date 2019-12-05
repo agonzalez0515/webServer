4df40ea1671b32d898c23941a8003d48e1cd2189
Angelica Gonzalez <angelica@8thlight.com>
2019-12-05 13:23:04 -0800

    Add socket handler tests and add close streams method

--- src/main/java/webserver/SocketHandler.java
+++ src/main/java/webserver/SocketHandler.java
@@ -9,10 +9,13 @@ import java.net.Socket;
 class SocketHandler implements Runnable {
     private BufferedReader in;
     private PrintWriter out;
+    private SocketIO io = new SocketIO();
+    private Socket client;
 
     public SocketHandler(Socket client) throws  IOException {
-        this.in = SocketIO.createSocketReader(client);
-        this.out = SocketIO.createSocketWriter(client);
+        this.in = io.createSocketReader(client);
+        this.out = io.createSocketWriter(client);
+        this.client = client;
     }
 
     public void run() {
@@ -23,6 +26,18 @@ class SocketHandler implements Runnable {
             response.setupDataToBeSent();
             response.send();
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally  {
+            closeStreams();
+          }
+    }
+
+    private void closeStreams() {
+        try {
+            if (out != null) { out.close(); }
+            if (in != null) { in.close(); }
+            client.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
