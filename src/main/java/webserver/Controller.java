0b831578a242203a752fd3616b652a8a97168efb
Angelica Gonzalez <angelica@8thlight.com>
2019-12-10 16:38:49 -0800

    Add controller for head and get requests

--- src/main/java/webserver/Controller.java
+++ src/main/java/webserver/Controller.java
@@ -3,19 +3,69 @@ package webserver;
 import java.io.IOException;
 
 public class Controller {
-    public String index() throws IOException {
-        return ResponseBody.getHtml("/index.html");
+    public String getIndex() throws IOException {
+        String body = ResponseBody.getHtml("/index.html");
+        return getResponseBuilder(200, body);
     }
 
-    public String healthCheck() throws IOException {
-        return ResponseBody.getHtml("/public/health-check.html");
+    public String getHealthCheck() throws IOException {
+        String body = ResponseBody.getHtml("/public/health-check.html");
+        return getResponseBuilder(200, body);
     }
 
-    public String listingList() throws IOException {
-        return ResponseBody.getHtml("/public/todo.html");
+    public String getNotFound() throws IOException {
+        String body = ResponseBody.getHtml("/404.html");
+        return getResponseBuilder(404, body);
     }
 
-    public String listingDetail(int id) throws IOException {
-        return ResponseBody.getHtml("/public/" + id +".html");
+    public String getListingList() throws IOException {
+        String body = ResponseBody.getHtml("/public/todo.html");
+        return getResponseBuilder(200, body);
+    }
+
+    public String getListingDetail(int id) throws IOException {
+        String body = ResponseBody.getHtml("/public/" + id +".html");
+        return getResponseBuilder(200, body);
+    }
+
+    public String headIndex() throws IOException {
+        String body = ResponseBody.getHtml("/index.html");
+        return headResponseBuilder(200, body);
+    }
+
+    public String headHealthCheck() throws IOException {
+        String body = ResponseBody.getHtml("/public/health-check.html");
+        return headResponseBuilder(200, body);
+    }
+
+    public String headNotFound() throws IOException {
+        String body = ResponseBody.getHtml("/404.html");
+        return headResponseBuilder(404, body);
+    }
+
+    public String headListingList() throws IOException {
+        String body = ResponseBody.getHtml("/public/todo.html");
+        return headResponseBuilder(200, body);
+    }
+
+    public String headListingDetail(int id) throws IOException {
+        String body = ResponseBody.getHtml("/public/" + id +".html");
+        return headResponseBuilder(200, body);
+    }
+
+    private String getResponseBuilder(int statusCode, String body) {
+        return new Response.Builder(statusCode)
+                .withContentType("Content-Type: text/html; charset=utf-8")
+                .withBodyLength(body.length())
+                .withBody(body)
+                .build();
+    }
+
+    private String headResponseBuilder(int statusCode, String body) {
+        return new Response.Builder(statusCode)
+                .withContentType("Content-Type: text/html; charset=utf-8")
+                .withBodyLength(body.length())
+                .build();
     }
 }
+
