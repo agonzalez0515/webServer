051f5f78dd3f39fcc6564312480996c30e8413f1
Angelica Gonzalez <angelica@8thlight.com>
2019-12-10 16:39:26 -0800

    Refactor Response object to return response string

--- src/test/java/webserver/ResponseTest.java
+++ src/test/java/webserver/ResponseTest.java
@@ -2,34 +2,28 @@ package webserver;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.core.IsNot.not;
-
 
 public class ResponseTest {
     ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    PrintWriter out = new PrintWriter(outContent, true);
 
 
     @Test
     public void  testSetsInitialResponseLineForOkMessage() throws IOException {
-        Response response = new Response.Builder(out, 200).withContentType("Content-Type: text/html; charset=utf-8").withBody("<!DOCTYPE html>").build();
-        response.send();
+        String response = new Response.Builder(200).withContentType("Content-Type: text/html; charset=utf-8").withBody("<!DOCTYPE html>").build();
 
-        assertThat(outContent.toString(), containsString("HTTP/1.1 200 OK") );
+        assertThat(response, containsString("HTTP/1.1 200 OK") );
     }
 
     @Test
     public void  testSetsInitialResponseLineForNotFoundMessage() throws IOException {
-        Response response = new Response.Builder(out, 404).withContentType("Content-Type: text/html; charset=utf-8").withBody("<!DOCTYPE html>").build();
-        response.send();
+        String response = new Response.Builder(404).withContentType("Content-Type: text/html; charset=utf-8").withBody("<!DOCTYPE html>").build();
 
-        assertThat(outContent.toString(), containsString("HTTP/1.1 404 Not Found"));
+        assertThat(response, containsString("HTTP/1.1 404 Not Found"));
     }
 }
 
