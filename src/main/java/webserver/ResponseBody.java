24c79bf9237505c6926f7de5c04744598800165d
Angelica Gonzalez <angelica@8thlight.com>
2019-12-10 16:38:26 -0800

    Rename htmlHandler class to responseBody

--- src/main/java/webserver/ResponseBody.java
+++ src/main/java/webserver/ResponseBody.java
@@ -9,7 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 
-public class HtmlHandler {
+public class ResponseBody {
   public static boolean fileExists(String path) {
     Path parsedPath = createFilePath(path);
     return Files.exists(parsedPath);
